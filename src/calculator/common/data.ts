6053f2fdef255f93a149c1867f8b93403938dd07
Dillan Prince <dillanprince@gmail.com>
2022-06-10 18:41:34 -0500

    add jeweler's chamber recipes

--- src/calculator/common/data.ts
+++ src/calculator/common/data.ts
@@ -13,6 +13,573 @@ export type DataType = {
   [itemName: string]: DataRowType;
 };
 
+export const jewelcraftingTableRecipes: DataType = {
+  "Bone Ring": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        Bone: 40,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        Bone: 30,
+      },
+    ],
+  },
+  "Blood Rose Ring": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Plant Fibre": 80,
+        "Blood Rose": 30,
+        "Bone Ring": 1,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Plant Fibre": 60,
+        "Blood Rose": 23,
+        "Bone Ring": 1,
+      },
+    ],
+  },
+  "Gravedigger Ring": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Grave Dust": 8,
+        "Mourning Lily": 40,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Grave Dust": 6,
+        "Mourning Lily": 30,
+      },
+    ],
+  },
+  "The Ember Chain": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Greater Blood Essence": 1,
+        "Crude Emerald": 4,
+        "Grave Dust": 8,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Greater Blood Essence": 1,
+        "Crude Emerald": 3,
+        "Grave Dust": 6,
+      },
+    ],
+  },
+  "The Frozen Eye": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Greater Blood Essence": 1,
+        "Crude Sapphire": 4,
+        "Grave Dust": 8,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Greater Blood Essence": 1,
+        "Crude Sapphire": 3,
+        "Grave Dust": 6,
+      },
+    ],
+  },
+  "Duskwatcher's Ring": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Greater Blood Essence": 1,
+        "Crude Topaz": 4,
+        "Grave Dust": 8,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Greater Blood Essence": 1,
+        "Crude Topaz": 3,
+        "Grave Dust": 6,
+      },
+    ],
+  },
+  "Mist Signet": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Greater Blood Essence": 1,
+        "Crude Miststone": 4,
+        "Grave Dust": 8,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Greater Blood Essence": 1,
+        "Crude Miststone": 3,
+        "Grave Dust": 6,
+      },
+    ],
+  },
+  "Scourge Pendant": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        Scourgestone: 1,
+        Glass: 12,
+        Sunflower: 60,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        Scourgestone: 1,
+        Glass: 9,
+        Sunflower: 45,
+      },
+    ],
+  },
+  "Ruby Pendant": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Regular Ruby": 4,
+        Scourgestone: 8,
+        "Scourge Pendant": 1,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Regular Ruby": 3,
+        Scourgestone: 6,
+        "Scourge Pendant": 1,
+      },
+    ],
+  },
+  "Sapphire Pendant": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Regular Sapphire": 4,
+        Scourgestone: 8,
+        "Scourge Pendant": 1,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Regular Sapphire": 3,
+        Scourgestone: 6,
+        "Scourge Pendant": 1,
+      },
+    ],
+  },
+  "Amethyst Pendant": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Regular Amethyst": 4,
+        Scourgestone: 8,
+        "Scourge Pendant": 1,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Regular Amethyst": 3,
+        Scourgestone: 6,
+        "Scourge Pendant": 1,
+      },
+    ],
+  },
+  "Emerald Pendant": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Regular Emerald": 4,
+        Scourgestone: 8,
+        "Scourge Pendant": 1,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Regular Emerald": 3,
+        Scourgestone: 6,
+        "Scourge Pendant": 1,
+      },
+    ],
+  },
+  "Misty Pendant": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Regular Miststone": 4,
+        Scourgestone: 8,
+        "Scourge Pendant": 1,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Regular Miststone": 3,
+        Scourgestone: 6,
+        "Scourge Pendant": 1,
+      },
+    ],
+  },
+  "Topaz Pendant": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Regular Topaz": 4,
+        Scourgestone: 8,
+        "Scourge Pendant": 1,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Regular Topaz": 3,
+        Scourgestone: 6,
+        "Scourge Pendant": 1,
+      },
+    ],
+  },
+  "Corrupted Artefact": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Golden Jewellery": 8,
+        "Spectral Dust": 8,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Golden Jewellery": 6,
+        "Spectral Dust": 6,
+      },
+    ],
+  },
+  "Shard of the Frozen Crypt": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Flawless Sapphire": 4,
+        "Gold Ingot": 8,
+        "Corrupted Artefact": 1,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Flawless Sapphire": 3,
+        "Gold Ingot": 6,
+        "Corrupted Artefact": 1,
+      },
+    ],
+  },
+  "Nightstone of the Beast": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Flawless Amethyst": 4,
+        "Gold Ingot": 8,
+        "Corrupted Artefact": 1,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Flawless Amethyst": 3,
+        "Gold Ingot": 6,
+        "Corrupted Artefact": 1,
+      },
+    ],
+  },
+  "Jewel of the Wicked Prophet": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Flawless Ruby": 4,
+        "Gold Ingot": 8,
+        "Corrupted Artefact": 1,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Flawless Ruby": 3,
+        "Gold Ingot": 6,
+        "Corrupted Artefact": 1,
+      },
+    ],
+  },
+  "Blood Key": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Gem dust": 400,
+        "Dark Silver Ingot": 28,
+        "Gold Ingot": 8,
+        "Primal Blood Essence": 4,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Gem dust": 300,
+        "Dark Silver Ingot": 21,
+        "Gold Ingot": 6,
+        "Primal Blood Essence": 3,
+      },
+    ],
+  },
+};
+
+export const gemCuttingTableRecipes: DataType = {
+  "Siege Golem Stone": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Stone Brick": 200,
+        "Gem dust": 200,
+        Scourgestone: 16,
+        "Greater Blood Essence": 12,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Stone Brick": 150,
+        "Gem dust": 150,
+        Scourgestone: 12,
+        "Greater Blood Essence": 9,
+      },
+    ],
+  },
+  "Regular Ruby": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Crude Ruby": 4,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Crude Ruby": 3,
+      },
+    ],
+  },
+  "Regular Sapphire": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Crude Sapphire": 4,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Crude Sapphire": 3,
+      },
+    ],
+  },
+  "Regular Emerald": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Crude Emerald": 4,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Crude Emerald": 3,
+      },
+    ],
+  },
+  "Regular Topaz": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Crude Topaz": 4,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Crude Topaz": 3,
+      },
+    ],
+  },
+  "Regular Miststone": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Crude Miststone": 4,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Crude Miststone": 3,
+      },
+    ],
+  },
+  "Regular Amethyst": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Crude Amethyst": 4,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Crude Amethyst": 3,
+      },
+    ],
+  },
+  "Flawless Ruby": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Regular Ruby": 4,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Regular Ruby": 3,
+      },
+    ],
+  },
+  "Flawless Sapphire": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Regular Sapphire": 4,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Regular Sapphire": 3,
+      },
+    ],
+  },
+  "Flawless Emerald": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Regular Emerald": 4,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Regular Emerald": 3,
+      },
+    ],
+  },
+  "Flawless Topaz": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Regular Topaz": 4,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Regular Topaz": 3,
+      },
+    ],
+  },
+  "Flawless Miststone": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Regular Miststone": 4,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Regular Miststone": 3,
+      },
+    ],
+  },
+  "Flawless Amethyst": {
+    source: "Jeweller's Chamber > Jewelcrafting Table",
+    fullPriceRecipes: [
+      {
+        amountMade: 1,
+        "Regular Amethyst": 4,
+      },
+    ],
+    discountedRecipes: [
+      {
+        amountMade: 1,
+        "Regular Amethyst": 3,
+      },
+    ],
+  },
+};
+
+export const jewellerChamberRecipes: DataType = {
+  ...jewelcraftingTableRecipes,
+  ...gemCuttingTableRecipes,
+};
+
 export const simpleWorkbenchRecipes: DataType = {
   "Reinforced Bone Sword": {
     source: "Forge > Simple Workbench",
@@ -2691,6 +3258,7 @@ export const alchemyLabRecipes: DataType = {
 };
 
 export const data: DataType = {
+  ...jewellerChamberRecipes,
   ...tailorRecipes,
   ...alchemyLabRecipes,
   ...forgeRecipes,
